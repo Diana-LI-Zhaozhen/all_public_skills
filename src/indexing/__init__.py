a46bc344017f72bd03a24d9a1691a1430580d9f8
zhaozhen.li <53840383+Diana-LI-Zhaozhen@users.noreply.github.com>
2026-04-12 16:50:16 +0800

    Delete src directory

--- src/indexing/__init__.py
+++
@@ -1,6 +0,0 @@
-from .vector_index import VectorIndex
-from .keyword_index import KeywordIndex
-from .table_store import TableStore
-from .metadata_index import MetadataIndex
-
-__all__ = ["VectorIndex", "KeywordIndex", "TableStore", "MetadataIndex"]
--- src/retrieval/__init__.py
+++
@@ -1,6 +0,0 @@
-from .router import QueryRouter
-from .hybrid_retriever import HybridRetriever
-from .reranker import Reranker
-from .table_rules import extract_sql_conditions
-
-__all__ = ["QueryRouter", "HybridRetriever", "Reranker", "extract_sql_conditions"]
